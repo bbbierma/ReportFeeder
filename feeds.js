5d0568dc3c4219c45b2c47021fee5f767a0f0f32
bbbierma <g.bbbierma@gmail.com>
2025-11-08 08:05:58 +0100

    Add FEEDS constant with RSS feed URLs

---
+++ feeds.js
@@ -0,0 +1,8 @@
+export const FEEDS = {
+  DraftReport:  'https://www.europarl.europa.eu/rss/doc/draft-reports/en.xml',
+  DraftOpinion: 'https://www.europarl.europa.eu/rss/doc/draft-opinions/en.xml',
+  AMsCMTE:      'https://www.europarl.europa.eu/rss/doc/amendments-committees/en.xml',
+  Opinions:     'https://www.europarl.europa.eu/rss/doc/opinions/en.xml',
+  Reports:      'https://www.europarl.europa.eu/rss/doc/reports/en.xml',
+  Agenda:       'https://www.europarl.europa.eu/rss/doc/agendas-committees/en.xml'
+};
